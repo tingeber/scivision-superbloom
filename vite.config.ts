2aefc11b72e061320c154337593919d3420b4f73
tin <tingeber@gmail.com>
2024-04-02 10:26:30 +0200

    added base to config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/",
 	resolve: {
 		alias: {
 			"node-fetch": "isomorphic-fetch",
